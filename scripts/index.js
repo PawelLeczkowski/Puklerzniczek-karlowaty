be2470407b212c3a6b94fc5c2878e03759a0de46
PawelLeczkowski <pawelleczkowski2005@gmail.com>
2026-07-13 16:47:29 +0200

    lines + kids creation

--- scripts/index.js
+++ scripts/index.js
@@ -1,28 +1,36 @@
 const main = document.getElementsByTagName("main")[0];
 
 // todo listen for changes
-let { all: tabs} = await chrome.storage.local.get('all');
+const { all: tabs} = await chrome.storage.local.get('all');
 console.log("onstorage", tabs);
 
 // first level
 // todo find and implement tree drawing algorithm
 for (const tab of tabs) {
-    CreateNode(tab.title, tab.url, tab.tabId);
-    if (tab.tabs.count > 0) {
+    const head = CreateNode(tab.title, tab.url, tab.tabId);
+    if (tab.tabs.length > 0) {
         // all next levels
-        CreateNodeRecursive(tab.title, tab.url, tab.tabId, tab.tabs);
+        CreateNodeRecursive(tab.title, tab.url, tab.tabId, tab.tabs, head);
     }
 }
 
 function CreateNode(title, url, tabId) {
-    let node = document.createElement("div");
+    const node = document.createElement("div");
     node.className = "node";
-    node.innerText = title;
-    var pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
+    let pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
 
-    node.addEventListener("click", (e) => {
+    const p = document.createElement('p');
+    p.textContent = title;
+    node.appendChild(p);
+
+    const button = document.createElement('button');
+    button.textContent = "Go to page";
+
+    button.addEventListener('click', () => {
         chrome.tabs.update(tabId, { active: true });
-    })
+    });
+
+    node.appendChild(button);
 
     node.onmousedown = dragMouseDown;
 
@@ -61,17 +69,39 @@ function CreateNode(title, url, tabId) {
     }
 }
 
-// todo implement kids creation
-function CreateNodeRecursive(title, url, tabId, tabs)
+function CreateNodeRecursive(title, url, tabId, tabs, parent)
 {
-    for (const tab in tabs) {
+    for (const tab of tabs) {
         // todo add lines between parents and kids
-        let kid = CreateNode(tab.title, tab.url, tab.tabId);
+        const kid = CreateNode(tab.title, tab.url, tab.tabId);
 
+        drawLine(parent, kid)
 
-
-        if (tab.tabs.count > 0) {
-            CreateNodeRecursive(tab.title, tab.url, tab.tabId);
+        if (tab.tabs.length > 0) {
+            CreateNodeRecursive(tab.title, tab.url, tab.tabId, tab.tabs, kid);
         }
     }
+}
+
+function drawLine(parent, kid) {
+    const svg = document.getElementById('line-container');
+
+    const line = document.createElementNS("http://www.w3.org/2000/svg", "line");
+
+    line.setAttribute('style', 'stroke:red;stroke-width:2');
+    svg.appendChild(line);
+
+    const rect1 = parent.getBoundingClientRect();
+    const rect2 = kid.getBoundingClientRect();
+    const svgRect = svg.getBoundingClientRect();
+
+    const x1 = rect1.left - svgRect.left + rect1.width / 2;
+    const y1 = rect1.top - svgRect.top + rect1.height / 2;
+    const x2 = rect2.left - svgRect.left + rect2.width / 2;
+    const y2 = rect2.top - svgRect.top + rect2.height / 2;
+
+    line.setAttribute('x1', x1);
+    line.setAttribute('y1', y1);
+    line.setAttribute('x2', x2);
+    line.setAttribute('y2', y2);
 }
